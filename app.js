6c5911cbf76e54d4ca3548a4f4020f06120a9c20
Alex <alexgarbuzov@ya.ru>
2022-09-24 11:05:28 +0300

    14

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const auth = require('./middlewares/auth');
+
+const {
+  createUser,
+  login,
+} = require('./controllers/user');
 
 const { PORT = 3000 } = process.env;
 
@@ -12,15 +18,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '62f7b9a52cdcd454268cde10',
-  };
-  next();
-});
+app.post('/signin', login);
+app.post('/signup', createUser);
+
+app.use(auth);
 
 app.use('/', require('./routes/user'));
-
 app.use('/', require('./routes/card'));
 
 app.use('*', (req, res) => {
--- controllers/user.js
+++ controllers/user.js
@@ -1,27 +1,57 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const HttpCodes = {
   BAD_REQUEST: 400,
+  Unauthorized: 401,
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
 };
 
 module.exports.createUser = (req, res) => {
-  User.create({
-    name: req.body.name,
-    about: req.body.about,
-    avatar: req.body.avatar,
-  })
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => User.create({
+      name: req.body.name,
+      about: req.body.about,
+      avatar: req.body.avatar,
+      email: req.body.email,
+      password: hash,
+    }))
     .then((user) => res.status(200).send({
       name: user.name,
       about: user.about,
       avatar: user.avatar,
+      email: user.email,
       _id: user._id,
     }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(HttpCodes.BAD_REQUEST).send({
           message: 'Переданы некорректные данные при создании пользователя',
+        });
+      } else {
+        res.status(HttpCodes.INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        'some-secret-key',
+        { expiresIn: '7d' },
+      );
+      res.send({ token });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(HttpCodes.Unauthorized).send({
+          message: 'Переданы некорректные данные при авторизации',
         });
       } else {
         res.status(HttpCodes.INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' });
