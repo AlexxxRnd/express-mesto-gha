7e70c4c634c1ee0625ef99777f0d17714e59a2ea
Alex <alexgarbuzov@ya.ru>
2022-09-26 22:34:39 +0300

    fix

--- controllers/card.js
+++ controllers/card.js
@@ -13,7 +13,7 @@ module.exports.createCard = (req, res, next) => {
     .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return next(new BadRequestError('Переданы неверные данные.'));
+        next(new BadRequestError('Переданы неверные данные.'));
       }
       return next(err);
     });
