a8c3739806f0a6834277c3976cebdb14aff39698
Alex <alexgarbuzov@ya.ru>
2022-09-26 21:55:24 +0300

    fix

--- routes/card.js
+++ routes/card.js
@@ -9,13 +9,12 @@ const {
 } = require('../controllers/card');
 
 const {
-  createCardValidation,
   cardIdValidation,
 } = require('../middlewares/validation');
 
 router.get('/cards', getCards);
 router.delete('/cards/:cardId', cardIdValidation, deleteCard);
-router.post('/cards', createCardValidation, createCard);
+router.post('/cards', createCard);
 router.put('/cards/:cardId/likes', cardIdValidation, likeCard);
 router.delete('/cards/:cardId/likes', cardIdValidation, unlikeCard);
 
