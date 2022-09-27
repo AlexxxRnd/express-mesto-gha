17e04689473c1a882568c4333be852b62b876a7f
Alex <alexgarbuzov@ya.ru>
2022-09-27 17:37:27 +0300

    fix

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const UnauthorizedError = require('../errors/UnauthorizedError');
-// const BadRequestError = require('../errors/BadRequestError');
+const BadRequestError = require('../errors/BadRequestError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,6 +22,7 @@ const userSchema = new mongoose.Schema({
       validator(v) {
         return /^https?:\/\/(www\.)?[a-zA-Z0-9\-.]{1,}\.[a-zA-Z]{1,4}[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]{1,}/.test(v);
       },
+      throw: BadRequestError('Неверный url'),
     },
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
