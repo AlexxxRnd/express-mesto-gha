8addf81a07ce89dd014c2b0882934e4a38df7dcc
Alex <alexgarbuzov@ya.ru>
2022-09-27 17:47:51 +0300

    fix

--- models/user.js
+++ models/user.js
@@ -21,7 +21,6 @@ const userSchema = new mongoose.Schema({
       validator(v) {
         return /^https?:\/\/(www\.)?[a-zA-Z0-9\-.]{1,}\.[a-zA-Z]{1,4}[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]{1,}/.test(v);
       },
-      message: (props) => `${props.value} не является URL`,
     },
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
